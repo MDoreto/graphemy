874c035b0a3df66b66a60934707fcaeb75cf98de
Matheus Doreto <matheus.alencar@prometeon.com>
2024-04-11 10:38:49 -0300

    :sparkles: import_files

--- examples/tutorial/models/member.py
+++ examples/tutorial/models/member.py
@@ -1,9 +1,9 @@
 from graphemy import Dl, Field, Graphemy
 
 
-class Album(Graphemy, table=True):
+class Member(Graphemy, table=True):
     band_id: int
     id: int = Field(primary_key=True)
     name: str
-    musics: list['Music'] = Dl(source='id', target='album_id')
+    role:str
     band: 'Band' = Dl(source='band_id', target='id')
--- graphemy/__init__.py
+++ graphemy/__init__.py
@@ -4,3 +4,13 @@ from graphemy.dl import Dl
 from graphemy.models import Graphemy
 from graphemy.router import GraphemyRouter
 from graphemy.setup import Setup
+import os
+def import_files(path):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.py') and file != '__init__.py':
+                module_name = os.path.splitext(file)[0]
+                module_path = os.path.join(root, module_name)
+                module_path_rel = os.path.relpath(os.path.join(root, module_name))
+                module_path = module_path_rel.replace(os.path.sep, '.')
+                exec(f'import {module_path}')
