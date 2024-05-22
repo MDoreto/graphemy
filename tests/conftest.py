26bbdd5d2252ff7a9f5f335630af4e0e1fe83404
MDoreto <matheusdoreto.md@gmail.com>
2024-05-22 02:33:55 -0300

    :test_tube: finished tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -97,3 +97,107 @@ async def client_async():
     Setup.async_engine = False
     Graphemy.metadata.clear()
     default_registry.dispose()
+
+
+@pytest.fixture(scope='module')
+def client_auth():
+    from fastapi import FastAPI
+    from fastapi.testclient import TestClient
+    from sqlmodel import Session, create_engine
+    from sqlmodel.pool import StaticPool
+
+    from graphemy import Dl, Field, Graphemy, GraphemyRouter, Setup
+
+    class Resource(Graphemy, table=True):
+        id: int | None = Field(primary_key=True, default=None)
+        name: str
+        category: str
+        owner_id: str | None
+        private_id: int | None
+        owner: 'Owner' = Dl(source='owner_id', target='id')
+        private: 'Private' = Dl(source='private_id', target='id')
+
+    class Owner(Graphemy, table=True):
+        id: str | None = Field(primary_key=True, default=None)
+        name: str
+        resources: list['Resource'] = Dl(source='id', target='owner_id')
+        privates: list['Private'] = Dl(source='id', target='owner_id')
+        keys: list['Key'] = Dl(source='id', target='owner_id')
+
+    class Private(Graphemy, table=True):
+        id: int | None = Field(primary_key=True, default=None)
+        description: str
+        owner_id: str
+        owner: 'Owner' = Dl(source='owner_id', target='id')
+
+    class Key(Graphemy, table=True):
+        id: int | None = Field(primary_key=True, default=None)
+        description: str
+        owner_id: str
+        owner: 'Owner' = Dl(source='owner_id', target='id')
+
+    engine = create_engine(
+        'sqlite://',
+        poolclass=StaticPool,
+        connect_args={'check_same_thread': False},
+    )
+    Graphemy.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        session.add(Owner(id='1', name='Center 1'))
+        session.add(
+            Resource(
+                id=1, name='Base 1', category='A', owner_id='1', private_id=1
+            )
+        )
+        session.add(
+            Resource(
+                id=2, name='Base 2', category='B', owner_id='1', private_id=1
+            )
+        )
+        session.add(
+            Resource(
+                id=3,
+                name='Base 3',
+                category='C',
+                owner_id='2',
+                private_id=None,
+            )
+        )
+        session.add(Private(id=1, description='Extra 1', owner_id='1'))
+        session.commit()
+
+    async def get_context(request, response):
+        user = {'categories': ['A', 'B'], 'classes': ['Resource', 'Owner']}
+        request.scope['user'] = user
+        return {'user': user}
+
+    def dl_filter(data, request):
+        user = request.scope['user']
+        if (
+            data
+            and isinstance(data, list)
+            and len(data) > 0
+            and isinstance(data[0], Resource)
+        ):
+            return [d for d in data if d.category in user['categories']]
+        return data
+
+    def query_filter(model, info):
+        if model.__name__ == 'Resource':
+            return model.category.in_(info.context['user']['categories'])
+        return True
+
+    async def permission_getter(module_class, context, request_type):
+        return module_class.__name__ in context['user']['classes']
+
+    app = FastAPI()
+    router = GraphemyRouter(
+        engine=engine,
+        dl_filter=dl_filter,
+        query_filter=query_filter,
+        permission_getter=permission_getter,
+        context_getter=get_context,
+    )
+    app.include_router(router, prefix='/graphql')
+    return TestClient(app)
---
+++ tests/test_auth.py
@@ -0,0 +1,148 @@
+def test_class_permission(client_auth):
+    response = client_auth.post(
+        '/graphql',
+        json={
+            'query': """query MyQuery {
+                privates {
+                    id
+                    description
+                }
+            }"""
+        },
+    )
+    assert response.status_code == 403
+    assert response.json() == {
+        'data': {'privates': []},
+        'errors': [
+            {
+                'message': "User don't have necessary permissions for this path",
+                'path': ['private'],
+            }
+        ],
+    }
+
+
+def test_permission_dl(client_auth):
+    response = client_auth.post(
+        '/graphql',
+        json={
+            'query': """query MyQuery {
+                owners {
+                    id
+                    name
+                    privates {
+                        id
+                        description
+                    }
+                    keys{
+                        id
+                        description
+                    }
+                }
+            }"""
+        },
+    )
+    assert response.status_code == 403
+    assert response.json() == {
+        'data': {
+            'owners': [
+                {'id': '1', 'name': 'Center 1', 'privates': [], 'keys': []}
+            ]
+        },
+        'errors': [
+            {
+                'message': "User don't have necessary permissions for this path",
+                'path': ['private', 'key'],
+            }
+        ],
+    }
+
+
+def test_permission_dl_one(client_auth):
+    response = client_auth.post(
+        '/graphql',
+        json={
+            'query': """query MyQuery {
+                resources {
+                    id
+                    name
+                    private {
+                        id
+                        description
+                    }
+                }
+            }"""
+        },
+    )
+    assert response.status_code == 403
+    assert response.json() == {
+        'data': {
+            'resources': [
+                {'id': 1, 'name': 'Base 1', 'private': None},
+                {'id': 2, 'name': 'Base 2', 'private': None},
+            ]
+        },
+        'errors': [
+            {
+                'message': "User don't have necessary permissions for this path",
+                'path': ['private'],
+            }
+        ],
+    }
+
+
+def test_query_permission(client_auth):
+    response = client_auth.post(
+        '/graphql',
+        json={
+            'query': """query MyQuery {
+                resources {
+                    id
+                    name
+                    category
+                }
+            }"""
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'data': {
+            'resources': [
+                {'category': 'A', 'id': 1, 'name': 'Base 1'},
+                {'category': 'B', 'id': 2, 'name': 'Base 2'},
+            ]
+        }
+    }
+
+
+def test_dl_filter(client_auth):
+    response = client_auth.post(
+        '/graphql',
+        json={
+            'query': """query MyQuery {
+                owners {
+                    id
+                    name
+                    resources{
+                        id
+                        category
+                    }
+                }
+            }"""
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'data': {
+            'owners': [
+                {
+                    'id': '1',
+                    'name': 'Center 1',
+                    'resources': [
+                        {'id': 1, 'category': 'A'},
+                        {'id': 2, 'category': 'B'},
+                    ],
+                }
+            ]
+        }
+    }
